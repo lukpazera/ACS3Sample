9971104bfd32fb84185346b9b84de5e5f0659f94
Lukasz Pazera <lukpazera@gmail.com>
2023-10-25 13:46:44 +0200

    Initial commit of a small subset of ACS3 source files.

---
+++ Scripts/rs/items/module_assm.py
@@ -0,0 +1,45 @@
+
+""" Module assembly item.
+
+    Copyright (C) 2017-2019 Lukasz Pazera.
+"""
+
+
+import lx
+from ..item import Item
+from ..const import *
+from ..component_setups.module import ModuleComponentSetup
+from ..items.module_root import ModuleRoot
+
+
+class ModuleAssembly(Item):
+    
+    descType = RigItemType.MODULE_ASSM
+    descUsername = 'Module Assembly Item'
+    descModoItemType = 'group'
+    descModoItemSubtype = 'assembly'
+    descDefaultName = 'Module Assembly'
+    descPackages = []
+
+    # -------- Public interface
+
+    def onAdd(self, subtype):
+        lx.eval('select.item {%s} set' % self.modoItem.id)
+        lx.eval('item.editorColor white')
+        
+    @property
+    def name(self):
+        """ Gets module assembly name.
+        
+        Module assembly doesn't have its own name, it takes it from module.
+        """
+        try:
+            modSetup = ModuleComponentSetup(self.modoItem)
+        except TypeError:
+            return ''
+        modRoot = ModuleRoot(modSetup.rootModoItem)
+        return modRoot.name
+
+    @name.setter
+    def name(self, name):
+        pass
